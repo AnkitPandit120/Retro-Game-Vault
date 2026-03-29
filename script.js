746d63d6fb5794ea5851b02476109c8aabbbe4dd
Vansh <techsfc120.zz@gmail.com>
2026-03-30 01:53:04 +0530

    showing name and image of fetched data

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 async function fetchData(){
     let res= await fetch(`https://api.rawg.io/api/games?key=0a4e17ae0a3c4454abc1fd8a2b7b5d50`)
     let data= await res.json()
-    console.log(data)
+    let game_list=data["results"]
+    let container = document.getElementById("games")
+
+    for (i in game_list){
+        // console.log(game_list[i]["name"])
+        // console.log(game_list[i]["background_image"])
+        let Game= document.createElement("div")
+        Game.innerHTML=`
+        <h2>${game_list[i]["name"]}</h2>
+        <img width="200px" height="200px" src="${game_list[i]["background_image"]}" alt="${game_list[i]["name"]}">
+        `
+        container.appendChild(Game)
+    }
 }
 fetchData()
