9c01b3418c145950d0143b02efb8a7125d920716
Vansh <techsfc120.zz@gmail.com>
2026-03-30 13:34:24 +0530

    adding button and rating

--- script.js
+++ script.js
@@ -2,17 +2,25 @@ async function fetchData(){
     let res= await fetch(`https://api.rawg.io/api/games?key=0a4e17ae0a3c4454abc1fd8a2b7b5d50`)
     let data= await res.json()
     let game_list=data["results"]
+    console.log(game_list)
 
     let container = document.getElementById("Games")
 
-    for (i in game_list){
-        let Game= document.createElement("div")
-        Game.classList.add("game-card")
-        Game.innerHTML=`
-        <h2>${game_list[i]["name"]}</h2>
-        <img src="${game_list[i]["background_image"]}" alt="${game_list[i]["name"]}">
-        `
-        container.appendChild(Game)
+
+    for (let game of game_list) {
+        let Game = document.createElement("div");
+        Game.classList.add("game-card");
+
+        Game.innerHTML = `
+            <img src="${game.background_image}" alt="${game.name}" />
+
+            <div class="game-info">
+                <p class="rating">⭐ ${game.rating}</p>
+                <h3>${game.name}</h3>
+                <button class="more-btn">More</button>
+            </div>
+        `;
+        container.appendChild(Game);
     }
 }
 fetchData()
