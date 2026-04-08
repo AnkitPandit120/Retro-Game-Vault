c868681b5d0e943344b1dd2b06e9e2a168e54c68
Vansh <techsfc120.zz@gmail.com>
2026-04-08 14:37:16 +0530

    adding load more button to load more cards

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-let allGames = []; 
+let allGames = [];
+let currentPage = 1;
 
-async function fetchData() {
-    let res = await fetch(`https://api.rawg.io/api/games?key=0a4e17ae0a3c4454abc1fd8a2b7b5d50`);
+async function fetchData(page = 1) {
+    let res = await fetch(`https://api.rawg.io/api/games?key=0a4e17ae0a3c4454abc1fd8a2b7b5d50&page=${page}`);
     let data = await res.json();
+    let newGames = data.results;
+    allGames = [...allGames, ...newGames];
 
-    allGames = data.results; 
     displayGames(allGames);
 }
 
@@ -18,7 +20,6 @@ function displayGames(games) {
 
         Game.innerHTML = `
             <img src="${game.background_image}" alt="${game.name}" />
-
             <div class="game-info">
                 <p class="rating">⭐️ ${game.rating}</p>
                 <h3 class="game-title">${game.name}</h3>
@@ -29,6 +30,7 @@ function displayGames(games) {
     }
 }
 
+
 function searchGames() {
     let input = document.getElementById("searchBar").value.toLowerCase();
 
@@ -37,6 +39,12 @@ function searchGames() {
     );
 
     displayGames(filtered);
+}
+
+
+function loadMore() {
+    currentPage++;
+    fetchData(currentPage);
 }
 
 fetchData();
