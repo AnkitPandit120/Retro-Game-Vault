1d38ee408926a9f58c297213cb0b9e60459ffd45
Vansh <techsfc120.zz@gmail.com>
2026-04-08 15:08:32 +0530

    adding dark and light mode button as a feature

--- script.js
+++ script.js
@@ -48,3 +48,15 @@ function loadMore() {
 }
 
 fetchData();
+
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        toggleBtn.textContent = "☀️ Light Mode";
+    } else {
+        toggleBtn.textContent = "🌙 Dark Mode";
+    }
+});
